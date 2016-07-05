f940e263c9e47e52adca17be7fd6046954d7700b
qpf <qpf@cg.com>
2016-07-05 17:10:20 +0800

    Initial Commit

---
+++ Design_Pattern/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Design_Pattern
+//
+//  Created by Yusone on 16/7/5.
+//  Copyright © 2016年 yusone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
